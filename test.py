9e524a9d8894713a5fa9f31b7e9688a55b3f514e
huangchaosuper <huangchaosuper@gmail.com>
2022-06-27 20:46:45 +0800

    fix

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import time
-from enhance.convert_onnx import ConvertOnnx
 from PIL import Image
 
+from convert.onnx_enhance import OnnxEnhance
+
 if __name__ == '__main__':
-    convert_onnx = ConvertOnnx("model/weights_chineseocr", "./model/weights_chineseocr")
+    convert_onnx = OnnxEnhance(r"./model/weights_base", r"./model/weights_base", r"./model/onnx")
     path = r'./assert'
     start_time = time.time()
-    convert_onnx.convert(Image.open("./assert/test.png"))
+    convert_onnx.infer(Image.open("./assert/test.png"))
     stop_time = time.time()
 
     print("average time = %f" % (stop_time - start_time))
